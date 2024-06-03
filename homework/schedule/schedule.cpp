4ac459affeafede2465d70b701ed8c1687b45b79
Szymon Markiewicz <s.markiewicz@wb.com.pl>
2024-06-03 14:52:57 +0200

    Style fixes 3

--- homework/schedule/schedule.cpp
+++ homework/schedule/schedule.cpp
@@ -1,14 +1,14 @@
 #include "schedule.hpp"
 
-void schedule(std::function<void()>func, std::chrono::seconds sec) {
+void schedule(std::function<void()> func, std::chrono::seconds sec) {
     std::this_thread::sleep_for(sec);
     func();
 }
-void schedule(std::function<void(int)>func, std::chrono::seconds sec, int a) {
+void schedule(std::function<void(int)> func, std::chrono::seconds sec, int a) {
     std::this_thread::sleep_for(sec);
     func(a);
 }
-void schedule(std::function<void(std::string, double)>func, std::chrono::seconds sec, std::string str, double a) {
+void schedule(std::function<void(std::string, double)> func, std::chrono::seconds sec, std::string str, double a) {
     std::this_thread::sleep_for(sec);
-    func(str,a);
+    func(str, a);
 }
