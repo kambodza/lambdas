a5266e581e88fa1eb23e3a5d6ed7b392f00d69a9
Szymon Markiewicz <s.markiewicz@wb.com.pl>
2024-06-03 14:50:22 +0200

    Another style fix

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -4,6 +4,6 @@
 #include <string>
 #include <thread>
 
-void schedule(std::function<void()>func, std::chrono::seconds sec);
-void schedule(std::function<void(int)>func, std::chrono::seconds sec, int a);
-void schedule(std::function<void(std::string, double)>func, std::chrono::seconds sec, std::string str, double a);
+void schedule(std::function<void()> func, std::chrono::seconds sec);
+void schedule(std::function<void(int)> func, std::chrono::seconds sec, int a);
+void schedule(std::function<void(std::string, double)> func, std::chrono::seconds sec, std::string str, double a);
