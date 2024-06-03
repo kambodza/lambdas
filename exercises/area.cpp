ee418bb2b97b7c1d38e0400b835ebba3dea0724a
Szymon Markiewicz <s.markiewicz@wb.com.pl>
2024-06-03 12:56:45 +0200

    exercises done

--- exercises/area.cpp
+++ exercises/area.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <string>
 #include <memory>
+#include <functional>
 
 // Change function `areaLessThan20` into lambda.
 // Then change it into `areaLessThanX`, which takes `x = 20` on a capture list.
@@ -31,9 +32,6 @@ public:
 using CirclePtr = shared_ptr<Circle>;
 using Collection = vector<CirclePtr>;
 
-bool areaLessThan20(CirclePtr s) {
-    return (s && s->getArea() < 20);
-}
 
 void printCollection(const Collection& collection) {
     for (const auto & it : collection) {
@@ -53,7 +51,7 @@ void printAreas(const Collection& collection) {
 
 void findFirstShapeMatchingPredicate(const Collection& collection,
                                      std::string info,
-                                     bool (*predicate)(CirclePtr s)) {
+                                     std::function<bool(CirclePtr)> predicate ){
     auto it = std::find_if(collection.begin(), collection.end(), predicate);
     if(it != collection.end()) {
         cout << "First shape matching predicate: " << info << endl;
@@ -84,9 +82,13 @@ int main() {
     cout << "Areas after sort: " << std::endl;
     printAreas(circles);
 
+    auto areaLessThanX = [x = 20](CirclePtr s) {
+        return (s && s->getArea() < 20);
+    };
+
     findFirstShapeMatchingPredicate(circles,
                                     "area less than 20",
-                                    areaLessThan20);
+                                    areaLessThanX);
 
     return 0;
 }
--- exercises/threeWays.cpp
+++ exercises/threeWays.cpp
@@ -8,8 +8,47 @@
 // * functor
 // * function
 
+class Functor
+{
+private:
+    /* data */
+public:
+    bool operator () (int num) const{ return ( num % 3 ) == 0; }
+};
+
+
+bool divisibleByThree( int num )
+{
+    return ( num % 3 ) == 0;
+}
+
 int main() {
     std::vector numbers = {18, 21, 36, 90, 27, 14, 103};
+
+    //function
+    std::cout << "Function:\n";
+    for( auto i = 0; i < std::distance(numbers.begin(), numbers.end()); i++ )
+    {
+        std::cout << "Number " << numbers[i] << (divisibleByThree(numbers[i] ) ? " is divisible ":" is not divisible ") << "by 3\n";
+    }
+
+    //lambda
+    std::cout << "Lambda:\n";
+    auto lambdaDivByThree = [](int num){
+        return ( num % 3 ) == 0;
+    };
+    for( auto i = 0; i < std::distance(numbers.begin(), numbers.end()); i++ )
+    {
+        std::cout << "Number " << numbers[i] << (lambdaDivByThree(numbers[i] ) ? " is divisible ":" is not divisible ") << "by 3\n";
+    }
+
+    //functor
+    std::cout << "Functor:\n";
+    Functor f;
+    for( auto i = 0; i < std::distance(numbers.begin(), numbers.end()); i++ )
+    {
+        std::cout << "Number " << numbers[i] << (f(numbers[i] ) ? " is divisible ":" is not divisible ") << "by 3\n";
+    }
 
     return 0;
 }
