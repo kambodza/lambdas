028fe93016d2c291f6e9f341ff21209b65142fa7
Szymon Markiewicz <s.markiewicz@wb.com.pl>
2024-06-03 14:46:08 +0200

    Formatting changed, parameter bug fixed

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -6,4 +6,4 @@
 
 void schedule(std::function<void()>func, std::chrono::seconds sec);
 void schedule(std::function<void(int)>func, std::chrono::seconds sec, int a);
-void schedule(std::function<void(std::string, double)>func, std::chrono::seconds sec, std::string str, int a);
+void schedule(std::function<void(std::string, double)>func, std::chrono::seconds sec, std::string str, double a);
