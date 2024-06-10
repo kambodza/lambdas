f1131082f145edb1638306a33302be7af97b8432
Szymon Markiewicz <s.markiewicz@wb.com.pl>
2024-06-10 07:46:05 +0200

    Changed config hoping for tests to complete

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -5,7 +5,7 @@
 #include <thread>
 
 template <typename Func, typename... A>
-void schedule(Func func, std::chrono::seconds sec, A... a){
+void schedule(Func func, std::chrono::seconds sec, A... a) {
     std::this_thread::sleep_for(sec);
     func(a...);
 }
